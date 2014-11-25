f91ef92b5eb68dcdf0dc789e504f69af45e42b07
Phoam <jeremiesrobitaille@gmail.com>
2014-11-25 16:44:42 -0500

    Resolve Merge Conflict

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,22 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141124230359) do
+ActiveRecord::Schema.define(version: 20141124231304) do
+
+  create_table "bags", force: true do |t|
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "lots", force: true do |t|
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "pots", force: true do |t|
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "users", force: true do |t|
     t.string   "name"
