477c5173e3d1091980045c90d98e95d3fbf34efa
Maxime Gauthier <maxime.gauthier88@gmail.com>
2014-12-03 18:57:40 -0500

    Seeds for Statuses

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+statuses = Status.create([
+  { name: :drying    },
+  { name: :curing    },
+  { name: :harvest   },
+  { name: :growing   },
+  { name: :destroyed }
+])
