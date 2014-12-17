c6f5652d62767e662f646719cc8c82ab3b6df62d
Phoam <jeremiesrobitaille@gmail.com>
2014-12-17 14:41:57 -0500

    Added field initial_weight To Bags & Lots

--- db/schema.rb
+++ db/schema.rb
@@ -11,13 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141217180133) do
+ActiveRecord::Schema.define(version: 20141217185108) do
 
   create_table "bags", force: true do |t|
     t.datetime "created_at"
     t.datetime "updated_at"
     t.integer  "lot_id"
     t.string   "name"
+    t.integer  "origin"
     t.integer  "weight"
   end
 
@@ -38,6 +39,7 @@ ActiveRecord::Schema.define(version: 20141217180133) do
     t.datetime "updated_at"
     t.integer  "bag_id"
     t.string   "name"
+    t.integer  "origin"
     t.integer  "weight"
   end
 
@@ -45,6 +47,9 @@ ActiveRecord::Schema.define(version: 20141217180133) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.string   "name"
+    t.string   "type"
+    t.string   "strain"
+    t.integer  "origin"
     t.integer  "weight"
   end
 
