66938ac651a0026a125ab1be7fd4b5e0c403fee7
Maxime Gauthier <maxime.gauthier88@gmail.com>
2014-12-19 14:01:49 -0500

    Fix sign out link

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,9 +3,12 @@ class User < ActiveRecord::Base
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
-
-  belongs_to :group, class_name: 'User::Group', foreign_key: 'user_group_id'
+  
   belongs_to :role, class_name: 'User::Role', foreign_key: 'user_role_id'
+  before_create :create_role, unless: :has_role?
+  
+  belongs_to :group, class_name: 'User::Group', foreign_key: 'user_group_id'
+  before_create :create_group, unless: :has_group?
 
   def to_s
     "#{ name.titleize unless name.nil? }"
@@ -18,4 +21,22 @@ class User < ActiveRecord::Base
   def manager?
     role.manager? || group.manager?
   end
+
+  private
+
+    def create_role
+      self.role = User::Role.user
+    end
+
+    def has_role?
+      !role.nil?
+    end
+
+    def create_group
+      self.group = User::Group.users
+    end
+
+    def has_group?
+      !group.nil?
+    end
 end
