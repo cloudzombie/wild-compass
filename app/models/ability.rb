4d40692ffd5f78e605c9f4cb6235a3d3fa4ae2a3
Maxime Gauthier <maxime.gauthier88@gmail.com>
2014-12-23 16:42:44 -0500

    Create ability and add send to lab button to view

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -3,12 +3,14 @@ class Ability
   def initialize(user)
     if user.admin?
       can :manage, :all
+      can :send_to_lab, Bag
     elsif user.manager?
       can :manage, Plant
       can :manage, Lot
       can :manage, Jar
       can :manage, Bag
       can :manage, Order
+      can :send_to_lab, Bag
     else
       can :read, Plant
       can :read, Lot
