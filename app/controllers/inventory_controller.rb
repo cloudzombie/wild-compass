e2719ccf3ec9d61de3356f617321e90e901be40c
Maxime 'biximilien' Gauthier <maxime.gauthier88@gmail.com>
2015-01-01 22:56:20 -0500

    ICS-4 #time 1h Split inventory into 2 tables, one for plants and one for lots

--- app/controllers/inventory_controller.rb
+++ app/controllers/inventory_controller.rb
@@ -1,4 +1,7 @@
 class InventoryController < ApplicationController
   expose(:inventory_lines)  { Bag.all + Plant.all + Jar.all + Lot.all }
+  expose(:bags) { Bag.all }
+  expose(:lots) { Lot.all }
+  
   def home; end
 end
