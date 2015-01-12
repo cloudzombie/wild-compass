0c22222cb2a1b0422d4ae891b15c55c4a4b65e51
Maxime 'biximilien' Gauthier <maxime.gauthier88@gmail.com>
2015-01-12 08:06:36 -0500

    Push containers fix

--- app/controllers/containers_controller.rb
+++ app/controllers/containers_controller.rb
@@ -23,19 +23,21 @@ class ContainersController < ApplicationController
   end
 
   def create
-    @container = Container.new(@container_params)
+    @container = Container.new(container_params)
+    if @container.weight.nil?
+      @container.weight = 0.0
+    else
+      @container.weight = @container.weight.to_d
+    end
     authorize! :create, @container
     
     Transaction.from( @container.lot ).to( @container ).take( @container.weight ).by( current_user ).commit
 
     respond_to do |format|
       if @container.save && @container.lot.save
-        
         format.html { redirect_to @container, notice: 'Container was successfully created.' }
-        format.json { render :show, status: :created, location: @container }
       else
         format.html { render :new }
-        format.json { render json: @container.errors, status: :unprocessable_entity }
       end
     end
   end 
@@ -51,6 +53,7 @@ class ContainersController < ApplicationController
   end
 
   private
+
     def set_container
       @container = Container.find(params[:id])
     end
