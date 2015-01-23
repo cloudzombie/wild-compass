d1ae0e9d8f8eec5576abbbed74f54dcb39c0747e
Maxime 'biximilien' Gauthier <maxime.gauthier88@gmail.com>
2015-01-23 16:14:31 -0500

    ICS-52 #time 1h 30m Fix search issues

--- app/models/concerns/searchable.rb
+++ app/models/concerns/searchable.rb
@@ -3,52 +3,24 @@ module Searchable
 
   module ClassMethods
     def search(search)
-      if search
-        queries = []
-        search_params = []
+      return all if search.nil? || search.empty?
+      query = where(id: search.split(',').map(&:strip).map(&:to_i))
+      
+      if query.empty?
+        query = where('name LIKE ?', "%#{search}%")
         
-        columns.each do |column|
-          next if column.name == :history_id
-          next if column.name == :datamatrix_text
-          next if column.name == :datamatrix_hash
-          case column.type
-          when :integer
-            queries << "#{column.name} = ?"
-            search_params << "#{search}".to_i
-          when :text, :string
-            queries << "#{column.name} LIKE ?"
-            search_params << "%#{search}%"
-          when :decimal
-            queries << "#{column.name} = ?"
-            search_params << "#{search}".to_d
-          when :datetime
-            begin
-              datetime = DateTime.parse("#{search}")
-              queries << "#{column.name} <= ?"
-              search_params << datetime.to_s
-            rescue
-              next
-            end
-          when :boolean
-            queries << "#{column.name} = ?"
-            if "#{search}".downcase == 'true'
-              search_params << true
-            elsif "#{search}".downcase == 'false'
-              search_params << false
-            else
-              search_params << nil
-            end
-          else
-            queries << "#{column.name} = ?"
-            search_params << "%#{search}%"
-          end
+        if query.empty?
+          none
+        else
+          query
         end
-        
-        where queries.join(' OR '), *search_params
 
       else
-        all
+        query
       end
+    rescue
+      all
     end
+
   end
 end
