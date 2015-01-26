54474c0f52b57c74e24be75aefdbfa1cb7a1c601
Maxime 'biximilien' Gauthier <maxime.gauthier88@gmail.com>
2015-01-26 16:24:08 -0500

    Fix error in bags sort

--- app/controllers/bags_controller.rb
+++ app/controllers/bags_controller.rb
@@ -9,9 +9,11 @@ class BagsController < ApplicationController
     elsif Bag.column_names.include? sort_column
       Bag.search(params[:search]).order(sort_column + ' ' + sort_direction).page(params[:page])
     elsif sort_column == 'strain'
-      Bag.search(params[:search]).joins(:strains).merge(Strain.order(acronym: sort_direction.to_sym)).uniq.page(params[:page])
+      Bag.search(params[:search]).joins(:strains).merge(Strain.order(acronym: sort_direction.to_sym)).page(params[:page])
     elsif sort_column == 'category'
-      Bag.search(params[:search]).joins(:containers).merge(Container.order(category: sort_direction.to_sym)).uniq.page(params[:page])
+      Bag.search(params[:search]).joins(:containers).merge(Container.order(category: sort_direction.to_sym)).page(params[:page])
+    else
+      Bag.search(params[:search]).page(params[:page])
     end
   end
 
