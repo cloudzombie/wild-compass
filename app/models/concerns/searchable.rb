4e69212c3f1f30fd8d496ebc70c9bedaf2d8555f
Maxime 'biximilien' Gauthier <maxime.gauthier88@gmail.com>
2015-01-27 20:42:28 -0500

    Fix search

--- app/models/concerns/searchable.rb
+++ app/models/concerns/searchable.rb
@@ -4,41 +4,36 @@ module Searchable
   module ClassMethods
     def search(search)
       return all if search.nil? || search.empty?
-      find_by_id
-    rescue
-      all
+      query = find_by_id(search)
+      if query.empty?
+        query = find_by_hash(search)
+        if query.empty?
+          query = find_by_name(search)
+          if query.empty?
+            none
+          else
+            query
+          end
+        else
+          query
+        end
+      else
+        query
+      end
     end
 
     private
 
-      def find_by_name
-        query = where('lower(name) LIKE ?', "%#{search}%".downcase)
-        
-        if query.empty?
-          none
-        else
-          query
-        end
+      def find_by_name(search)
+        where('lower(name) LIKE ?', "%#{search}%".downcase)
       end
 
-      def find_by_id
+      def find_by_id(search)
         query = where(id: search.split(',').map(&:strip))
-      
-        if query.empty?
-          find_by_hash
-        else
-          query
-        end
       end
 
-      def find_by_hash
+      def find_by_hash(search)
         query = where(datamatrix_hash: search)
-      
-        if query.empty?
-          find_by_name
-        else
-          query
-        end
       end
 
   end
