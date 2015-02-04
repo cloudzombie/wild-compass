ebbf6239733b24f6113c57ddd6909ae1a50a1dd4
Maxime 'biximilien' Gauthier <maxime.gauthier88@gmail.com>
2015-02-04 10:49:21 -0500

    Fix container sorts

--- app/controllers/concerns/find_sortable.rb
+++ app/controllers/concerns/find_sortable.rb
@@ -2,15 +2,39 @@ module FindSortable
   extend ActiveSupport::Concern
 
   def sort(resource)
-    case sort_column      
-    when 'name', :name
-      resource.search(params[:search]).order('LENGTH(name), name ' + sort_direction).page(params[:page])
-    when 'strain', :strain
-      resource.search(params[:search]).joins(:strains).merge(Strain.order(acronym: sort_direction.to_sym)).page(params[:page])
-    when 'category', :category
-      resource.search(params[:search]).joins(:container).merge(Container.order(category: sort_direction.to_sym)).page(params[:page])
+    case resource
+
+    when Bag.class
+      case sort_column      
+      when 'name', :name
+        resource.select('DISTINCT(bags.id), bags.*')
+                .search(params[:search])
+                .order('LENGTH(bags.name), bags.name ' + sort_direction)
+                .page(params[:page])
+      
+      when 'strain', :strain
+        resource.select('DISTINCT(bags.id), bags.*')
+                .search(params[:search])
+                .joins(:strains)
+                .merge( Strain.order(acronym: sort_direction.to_sym))
+                .page(params[:page])
+      
+      when 'category', :category
+        resource.select('DISTINCT(bags.id), bags.*')
+                .search(params[:search])
+                .joins(:container)
+                .merge( Container.order(category: sort_direction.to_sym))
+                .page(params[:page])
+      
+      else
+        resource.select('DISTINCT(bags.id), bags.*')
+                .search(params[:search])
+                .order("bags.#{sort_column}" + ' ' + sort_direction)
+                .page(params[:page])
+      end
+    
     else
-      resource.search(params[:search]).order(sort_column + ' ' + sort_direction).page(params[:page])
+      raise "Sort is not implemented for #{resource} yet..."
     end
   end
 
