7d83e449c291fb5ead94d5bdfeae47f5c8e160e9
Maxime 'biximilien' Gauthier <maxime.gauthier88@gmail.com>
2015-02-05 13:32:57 -0500

    ICS-67 #time 1h 30m Add bags status to bags

--- db/migrate/20150205182033_create_bags_statuses.rb
+++ db/migrate/20150205182033_create_bags_statuses.rb
@@ -1,9 +1,11 @@
 class CreateBagsStatuses < ActiveRecord::Migration
   def change
     create_table :bags_statuses do |t|
-      t.string :name
+      t.string :name, null: false, default: ''
 
       t.timestamps
     end
+
+    add_index :bags_statuses, :name, unique: true
   end
 end
