adadce59a44bfb1b4ac9896828d89f8a1e1985fa
Maxime 'biximilien' Gauthier <maxime.gauthier88@gmail.com>
2015-02-12 13:58:14 -0500

    ICS-84 #time 1h Plant report

--- app/controllers/inventory/seeds_controller.rb
+++ app/controllers/inventory/seeds_controller.rb
@@ -1,3 +1,19 @@
 class Inventory::SeedsController < InventoryController
-  expose(:seeds) { Seed.all }
+  
+  helper_method :sort_column, :sort_direction
+
+  expose(:seeds) { Seed.sort(sort_column, sort_direction) }
+
+  private
+
+    # Set column to sort in order.
+    def sort_column
+      %w(name initial_weight current_weight created_at updated_at).include?(params[:sort]) ? params[:sort] : 'id'
+    end
+
+    # Set sort direction to ascending or descending.
+    def sort_direction
+      %w(asc desc).include?(params[:direction]) ? params[:direction] : 'asc'
+    end
+
 end
