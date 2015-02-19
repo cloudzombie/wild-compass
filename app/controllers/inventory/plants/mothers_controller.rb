d987a770634d5484c14748c2561aae8f23f7b7b8
Maxime 'biximilien' Gauthier <maxime.gauthier88@gmail.com>
2015-02-19 15:26:39 -0500

    ICS-147 #time 2h More work on filter. Try to fix plants not saving

--- app/controllers/inventory/plants/mothers_controller.rb
+++ app/controllers/inventory/plants/mothers_controller.rb
@@ -2,6 +2,16 @@ class Inventory::Plants::MothersController < InventoryController
 
   include SetSortable
 
-  expose(:mothers) { Plant::MotherPlant.sort(sort_column, sort_direction) }
+  expose(:mothers) { Plant::MotherPlant.filter(filter_params).sort(sort_column, sort_direction) }
+
+  private
+
+    def filter_params
+      if params[:filter].nil?
+        {}
+      else
+        params.require(:filter).permit(:id, :strain_id, :format_id, :status_id)
+      end
+    end
   
 end
