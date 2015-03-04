8835712dbdf1ca668442719683983ded066c13bb
Maxime 'biximilien' Gauthier <maxime.gauthier88@gmail.com>
2015-03-04 16:33:30 -0500

    ICS-156 #time 1h Updated some fixtures and tests

--- app/models/user/group.rb
+++ app/models/user/group.rb
@@ -1,4 +1,26 @@
 class User::Group < ActiveRecord::Base
+
+  def self.users
+    find_by(name: 'users')
+  end
+
+  def self.managers
+    find_by(name: 'managers')
+  end
+
+  def self.admins
+    find_by(name: 'admins')
+  end
+
+  def self.super_users
+    find_by(name: 'super_users')
+  end
+
+  USERS       = User::Group.users
+  MANAGERS    = User::Group.managers
+  ADMINS      = User::Group.admins
+  SUPER_USERS = User::Group.super_users
+
   has_many :users
 
   belongs_to :role, class_name: 'User::Group::Role', foreign_key: 'user_group_role_id'
@@ -9,22 +31,6 @@ class User::Group < ActiveRecord::Base
 
   def manager?
     role.manager? || false
-  end
-
-  def self.users
-    find(1)
-  end
-
-  def self.managers
-    find(2)
-  end
-
-  def self.admins
-    find(3)
-  end
-
-  def self.super_users
-    find(4)
   end
 
   validates :name, presence: true
--- test/controllers/bags_controller_test.rb
+++ test/controllers/bags_controller_test.rb
@@ -1,45 +1,62 @@
 require 'test_helper'
 
 class BagsControllerTest < ActionController::TestCase
+  include Devise::TestHelpers
+
   setup do
-    @bag = bags(:one)
+    @bag = bags(:bag_1)
+    @user = users(:su_admin)
   end
 
   test "should get index" do
+    sign_in :user, @user
+    
     get :index
     assert_response :success
     assert_not_nil assigns(:bags)
   end
 
   test "should get new" do
+    sign_in :user, @user
+
     get :new
     assert_response :success
   end
 
   test "should create bag" do
+    sign_in :user, @user
+
     assert_difference('Bag.count') do
-      post :create, bag: {  }
+      post :create, bag: { initial_weight: 500.0, current_weight: 500.0 }
     end
 
     assert_redirected_to bag_path(assigns(:bag))
   end
 
   test "should show bag" do
+    sign_in :user, @user
+
     get :show, id: @bag
     assert_response :success
   end
 
   test "should get edit" do
+    sign_in :user, @user
+
     get :edit, id: @bag
     assert_response :success
   end
 
   test "should update bag" do
+    sign_in :user, @user
+
     patch :update, id: @bag, bag: {  }
     assert_redirected_to bag_path(assigns(:bag))
   end
 
   test "should destroy bag" do
+    sign_in :user, @user
+    
     assert_difference('Bag.count', -1) do
       delete :destroy, id: @bag
     end
