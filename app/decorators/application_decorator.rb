8720aa84b20e56ed95d4b9fa710fdb215dd6aa1d
Maxime 'biximilien' Gauthier <maxime.gauthier88@gmail.com>
2015-04-10 15:17:49 -0400

    Add actions for plants and orders

--- app/decorators/application_decorator.rb
+++ app/decorators/application_decorator.rb
@@ -2,6 +2,24 @@ class ApplicationDecorator < Draper::Decorator
 
   def self.collection_decorator_class                                              
     PaginatingDecorator                                                            
-  end 
+  end
+
+  def actions
+    h.content_tag :div, { role: 'group', class: 'btn-group' } do
+      [ menu_button, menu ].join.html_safe
+    end
+  end
+
+  def link_to_if_can?(action, resource, *content)
+    h.link_to_if h.can?(action, resource), *content
+  end
+
+  private
+
+    def menu_button
+      h.content_tag :button, { type: 'button', class: "btn-xs btn-primary dropdown-toggle", data: { toggle: 'dropdown', aria: { expanded: false }}} do
+        [ "Actions ", h.content_tag(:span, nil, class: 'caret') ].join.html_safe
+      end
+    end
   
 end
--- app/decorators/plant_decorator.rb
+++ app/decorators/plant_decorator.rb
@@ -4,7 +4,20 @@ class PlantDecorator < ApplicationDecorator
 
   delegate_all
 
-  def actions
+  def menu
+    h.content_tag :ul, role: 'menu', class: 'dropdown-menu pull-right' do
+      [ h.content_tag(:li, label), h.content_tag(:li, update) ].join.html_safe
+    end
   end
+
+  private
+
+    def label
+      link_to_if_can? :label, Plant, 'Label', h.label_plant_path(model), class: "text-green"
+    end
+
+    def update
+      link_to_if_can? :update, Plant, 'Edit', h.edit_plant_path(model), class: "text-yellow"
+    end
 
 end
